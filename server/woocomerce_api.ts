98df89ac2265dbc47d51c35fc66ef7093757b4f0
WebDev-dz <webdevdz2@gmail.com>
2025-06-23 19:51:13 +0000

    change url and keys

--- server/woocomerce_api.ts
+++ server/woocomerce_api.ts
@@ -7,9 +7,9 @@ const consumerKey = process.env.WOOCOMMERCE_CONSUMER_KEY ?? "something-random";
 const consumerSecret = process.env.WOOCOMMERCE_CONSUMER_SECRET ?? "something-random";
 
 export const api = new WooCommerceRestApi({
-  url,
-  consumerKey,
-  consumerSecret,
+  url:"https://sorashop.tahnut-shop.store/",
+  consumerKey:"ck_cdc57d3beba114183c621a27588d03407f092f5f",
+  consumerSecret:"cs_6cb38f13fc64b85ddf89e8845db12f9941e8a5f0",
   version: "wc/v3",
   
 })
