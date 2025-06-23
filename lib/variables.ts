47c951ae7bebfcff80840247c7e2a85949761e00
WebDev-dz <webdevdz2@gmail.com>
2025-06-23 11:18:48 +0000

    change variables

--- lib/variables.ts
+++ lib/variables.ts
@@ -13,12 +13,12 @@ export interface Store {
 }
 
 export const store: Store = {
-  name: process.env.STORE_NAME || "Store Name",
-  logo: process.env.STORE_LOGO || "/store.logo.png",
-  phone: process.env.STORE_PHONE_NUMBER || "+213 000 00 00 00",
-  address: process.env.STORE_ADDRESS || "Address",
-  email: process.env.STORE_EMAIL || "contact@example.com",
-  facebook: process.env.STORE_FACEBOOK || "https://web.facebook.com/profile.php?id=100067677527627"
+  name: "Sora Shop",
+  logo: "/store.logo.png",
+  phone: "+213 000 00 00 00",
+  address: "Constantine",
+  email:  "sorashop@tahnut-shop.com",
+  facebook:  "https://web.facebook.com/profile.php?id=100067677527627"
 };
 
 export const navItems: NavItem[] = [
